d43e0f543b6e1bffa171d6269cc54108e188075d
Agustin Lorenzi <agustinlorenzi6@gmail.com>
2022-12-22 19:25:55 -0300

    ejercicio4

---
+++ script.js
@@ -0,0 +1,13 @@
+//4. JavaScript ES6: Dados los siguientes array, imprimir por consola los elementos del array “y” 
+//que no se encuentran en el array “x” utilizando para tal fin una única línea de código. 
+//const x = ["n", "bro", "c", "|"]; const y = ["d", "n", "l", "bro", "g"];
+
+const y = ["d", "n", "l", "bro", "g"];
+
+const x = ["n", "bro", "c", "|"];
+
+y.forEach((num) => {let incluye = x.includes(num); incluye === false ? console.log(num):("")})  
+  
+   
+
+
